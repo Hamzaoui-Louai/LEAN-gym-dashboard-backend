01f6a66527b04af49762ab06c288ef12f2dd24fe
Hamzaoui-Louai <l_hamzaoui@estin.dz>
2026-08-07 15:41:07 +0100

    a bit more tweaking to fix render deployment and integration with supabase

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/up', function () {
+    return response()->json([
+        'status' => 'ok',
+        'services' => [
+            'database' => DB::connection()->getPdo() !== null,
+        ],
+    ]);
 });
